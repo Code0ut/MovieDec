76007b82e5bc78e33ce4ec8ba2fce05b4c32208d
Code0ut <omkarkalburgi01@gmail.com>
2025-11-05 09:44:59 +0530

    Chabged project structure

---
+++ config.py
@@ -0,0 +1,9 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'database': 'movie',
+    'user': 'postgres',
+    'password': 'yourPass'
+}
+
+SECRET_KEY = "12345"
+ALGORITHM = "HS256"
--- project/config.py
+++
@@ -1,9 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'database': 'movie',
-    'user': 'postgres',
-    'password': 'yourPass'
-}
-
-SECRET_KEY = "12345"
-ALGORITHM = "HS256"
